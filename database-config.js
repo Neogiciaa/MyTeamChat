4ad1304a81b302e44fa32cefdf54e00d2ff91fd2
Neogiciaa <chevalier.stephen@hotmail.com>
2024-10-16 13:56:16 +0200

    Database configuration with dotenv and mysql2

--- database-config.js
+++ database-config.js
@@ -1,11 +1,13 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connexion = await mysql.createConnection({
+const connection = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  name: process.env.DB_NAME,
+  database: process.env.DB_NAME
 })
+
+export default connection;
