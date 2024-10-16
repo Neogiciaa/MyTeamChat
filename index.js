c0763a19d1d0f36a545cbdfb38ad3eabc7b9f8a5
Neogiciaa <chevalier.stephen@hotmail.com>
2024-10-16 14:01:01 +0200

    Added express configuration

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello my team chat !");
+import connection from "./database-config.js";
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.API_PORT;
+const app = express();
+
+app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
