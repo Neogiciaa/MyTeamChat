8b1502804f637de208255cb01033bd3ccedbf3b9
Neogiciaa <chevalier.stephen@hotmail.com>
2024-10-16 15:59:23 +0200

    Added bcrypt security to hash user password and checkIfUserAlreadyExists function to avoid duplicated user accounts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import connection from "./database-config.js";
 import express from 'express';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 
@@ -12,22 +13,33 @@ app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
 
 async function createUser(name, email, password) {
   try {
-    await connection.query(`INSERT INTO User(name, email, password) VALUES (?, ?, ?)`, [name, email, password]);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connection.query(`INSERT INTO User(name, email, password) VALUES (?, ?, ?)`, [name, email, hashedPassword]);
   } catch (error) {
     console.log(error);
   }
 }
 
+async function checkIfUserAlreadyExists(email) {
+  const [result] = await connection.query(`SELECT * FROM User WHERE email = ?`, [email]);
+  return result.length > 0;
+
+}
+
 app.get("/", (req, res) => { res.send("Welcome to my team chat !"); });
 
 app.post("/register", async (req, res) => {
-  const {name, email, password} = req.query;
+  const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).send("Invalid credentials");
   }
 
-  // TODO Check if mail already exists or not before creating account !
+  const userExists = await checkIfUserAlreadyExists(email);
+
+  if (userExists) {
+    return res.status(400).send("User already exists");
+  }
 
   try {
     await createUser(name, email, password);
